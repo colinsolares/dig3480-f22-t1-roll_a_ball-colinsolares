fb380b9c1325d9d1fa8744e4b4ebd51d38f30c99
colinsolares <111911790+colinsolares@users.noreply.github.com>
2022-09-15 23:34:07 +0000

    Sep 15, 2022, 7:34 PM

--- Assets/Scripts/BoxMover.cs
+++ Assets/Scripts/BoxMover.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxMover : MonoBehaviour
+// Transforms to act as start and end markers for the journey.
+public Transform startMarker;
+public Transform endMarker;
+
+// Movement speed in units/sec.
+public float speed = 1.0F;
+
+// Time when the movement started.
+private float startTime;
+
+// Total distance between the markers.
+private float journeyLength;
 {
     // Start is called before the first frame update
     void Start()
