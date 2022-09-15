4750fe44f0053ebeba3ca1b94ee53fe0f8aa1a5b
colinsolares <111911790+colinsolares@users.noreply.github.com>
2022-09-15 23:36:44 +0000

    Sep 15, 2022, 7:36 PM

--- Assets/Scripts/BoxMover.cs
+++ Assets/Scripts/BoxMover.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxMover : MonoBehaviour
-// Transforms to act as start and end markers for the journey.
-public Transform startMarker;
-public Transform endMarker;
-
-// Movement speed in units/sec.
-public float speed = 1.0F;
-
-// Time when the movement started.
-private float startTime;
-
-// Total distance between the markers.
-private float journeyLength;
 {
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform EndMarker;
+
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +24,19 @@ private float journeyLength;
         startTime = Time.time;
 
         // Calculate the journey length.
-        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+        journeyLength = Vector3.Distance(startMarker.position, EndMarker.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Distance moved = time * speed.
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed = current distance divided by total distance.
+        float fracJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers and pingpong the movement.
+        transform.position = Vector3.Lerp(startMarker.position, EndMarker.position, Mathf.PingPong(fracJourney, 1));
     }
 }
